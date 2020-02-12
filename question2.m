d7b7d03c71b5d32644e4730c3608ae22e52bb008
AbbBug <git@samsoasis.com>
2020-02-11 18:35:59 -0800

    Update question2.m

--- question2.m
+++ question2.m
@@ -1,12 +1,12 @@
 % This is the code for question number 2. This code is slow
-iterations = 50; % length of time you want to take.
+iterations = 100; % length of time you want to take.
 left = -1.8;
 right = 1.8;
 bottom = -1.8;
 top = 1.8;
-xRes = 300;
-yRes = 300;
-f = 'z^2 -0.123 - 0.745i';
+xRes = 600;
+yRes = 600;
+f = 'z^2 -1.25';
 M = filledJulia(iterations,left,right,bottom,top,xRes,yRes, f);
 image([left right], [bottom top], M),
 axis xy
