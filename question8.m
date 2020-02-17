a1376fa50d2d878a5d5bc60802bacafd799d7f37
AbbBug <git@samsoasis.com>
2020-02-16 19:21:24 -0800

    Question number 8 without colors yet

---
+++ question8.m
@@ -0,0 +1,12 @@
+% This is the code for question number 2. This code is slow
+iterations = 100; % length of time you want to take.
+left = -1.8;
+right = 1.8;
+bottom = -1.8;
+top = 1.8;
+xRes = 5000;
+yRes = 5000;
+z = - 0.5;
+M = Mandelbrot(iterations,left,right,bottom,top,xRes,yRes, z);
+image([left right], [bottom top], M),
+axis xy
